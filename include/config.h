3b0088400599e67caf0bdce2a2ca32e499e2f6b9
nanase <kms1tt0@gmail.com>
2026-04-03 20:24:05 +0300

    code optimization

--- include/config.h
+++ include/config.h
@@ -1,15 +1,22 @@
 #pragma once
 #include <Arduino.h>
 
-#define SCREEN_WIDTH     128
-#define SCREEN_HEIGHT     64
-#define OLED_RESET        -1
-#define OLED_ADDRESS    0x3C
+#define SCREEN_WIDTH        128
+#define SCREEN_HEIGHT        64
+#define OLED_RESET           -1
+#define OLED_ADDRESS       0x3C
 
-#define NTP_SERVER       "pool.ntp.org"
-#define NTP_OFFSET       10800
-#define NTP_INTERVAL     60000
+#define NTP_SERVER        "pool.ntp.org"
+#define NTP_OFFSET         10800
+#define NTP_INTERVAL       60000
 
-#define WEATHER_UPDATE_MS 600000
-#define CLOCK_UPDATE_MS    1000
-#define WEATHER_FADE_STEPS   16
+#define WEATHER_UPDATE_MS  600000
+#define CLOCK_UPDATE_MS      1000
+#define WEATHER_FADE_STEPS     16
+#define WEATHER_FADE_TICK_MS   40
+
+#define WEATHER_ICON_W        10
+#define WIFI_RECONNECT_MS  30000
+
+#define AP_SSID    "modular"
+#define MDNS_NAME  "modular"
--- include/settings.h
+++ include/settings.h
@@ -2,31 +2,21 @@
 #include <Arduino.h>
 #include <Preferences.h>
 
-#define AP_SSID     "modular"
-#define MDNS_NAME   "modular"
-
 struct AppSettings {
     String wifiSSID;
     String wifiPassword;
     String owmApiKey;
     String city;
 
-    int    widgetClockX;
-    int    widgetClockY;
-    int    widgetDateX;
-    int    widgetDateY;
-    int    widgetWeatherX;
-    int    widgetWeatherY;
+    int  widgetClockX,   widgetClockY,   widgetClockSize;
+    int  widgetDateX,    widgetDateY,    widgetDateSize;
+    int  widgetWeatherX, widgetWeatherY, widgetWeatherSize;
 
-    int    widgetClockSize;
-    int    widgetDateSize;
-    int    widgetWeatherSize;
+    bool widgetClockVisible;
+    bool widgetDateVisible;
+    bool widgetWeatherVisible;
 
-    bool   widgetClockVisible;
-    bool   widgetDateVisible;
-    bool   widgetWeatherVisible;
-
-    int    brightness;
+    int  brightness;
 };
 
 class SettingsManager {
@@ -36,10 +26,10 @@ public:
     void load() {
         Preferences p;
         p.begin("modular", true);
-        data.wifiSSID       = p.getString("ssid",    "");
-        data.wifiPassword   = p.getString("pass",    "");
-        data.owmApiKey      = p.getString("owmkey",  "");
-        data.city           = p.getString("city",    "new-york");
+        data.wifiSSID     = p.getString("ssid",   "");
+        data.wifiPassword = p.getString("pass",   "");
+        data.owmApiKey    = p.getString("owmkey", "");
+        data.city         = p.getString("city",   "new-york");
 
         data.widgetClockX   = p.getInt("cx",  14);
         data.widgetClockY   = p.getInt("cy",  10);
@@ -48,9 +38,9 @@ public:
         data.widgetWeatherX = p.getInt("wx",  10);
         data.widgetWeatherY = p.getInt("wy",  52);
 
-        data.widgetClockSize    = p.getInt("csz", 3);
-        data.widgetDateSize     = p.getInt("dsz", 1);
-        data.widgetWeatherSize  = p.getInt("wsz", 1);
+        data.widgetClockSize   = p.getInt("csz", 3);
+        data.widgetDateSize    = p.getInt("dsz", 1);
+        data.widgetWeatherSize = p.getInt("wsz", 1);
 
         data.widgetClockVisible   = p.getBool("cvis", true);
         data.widgetDateVisible    = p.getBool("dvis", true);
@@ -68,12 +58,12 @@ public:
         p.putString("owmkey", data.owmApiKey);
         p.putString("city",   data.city);
 
-        p.putInt("cx", data.widgetClockX);
-        p.putInt("cy", data.widgetClockY);
-        p.putInt("dx", data.widgetDateX);
-        p.putInt("dy", data.widgetDateY);
-        p.putInt("wx", data.widgetWeatherX);
-        p.putInt("wy", data.widgetWeatherY);
+        p.putInt("cx",  data.widgetClockX);
+        p.putInt("cy",  data.widgetClockY);
+        p.putInt("dx",  data.widgetDateX);
+        p.putInt("dy",  data.widgetDateY);
+        p.putInt("wx",  data.widgetWeatherX);
+        p.putInt("wy",  data.widgetWeatherY);
 
         p.putInt("csz", data.widgetClockSize);
         p.putInt("dsz", data.widgetDateSize);
@@ -82,12 +72,11 @@ public:
         p.putBool("cvis", data.widgetClockVisible);
         p.putBool("dvis", data.widgetDateVisible);
         p.putBool("wvis", data.widgetWeatherVisible);
-
         p.putInt("bright", data.brightness);
         p.end();
     }
 
-    bool isConfigured() {
+    bool isConfigured() const {
         return data.wifiSSID.length() > 0;
     }
 };
